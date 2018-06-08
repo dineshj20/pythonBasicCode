3feaacd98f6558e646694d213471f86ea76286e4
Dinesh Jain <cooldj14@gmail.com>
2018-06-09 00:19:24 +0530

    First Commit of Fibonacci Series
    We will start coding in next commit

---
+++ fibonacciSeries.py
@@ -0,0 +1,3 @@
+#Now Let us start with the second program which is about Fibonacci series
+#Except for the first two terms of the sequence, every other term is the sum of the previous two terms
+# 0 1 1 2 3 5 8 13 21 34  . . . . and so on
