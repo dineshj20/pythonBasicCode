0f27cb8606364e5cd70d902f082af46936d145e8
Dinesh Jain <cooldj14@gmail.com>
2018-06-14 16:07:03 +0530

    To chech whether given number is palindrome
    Finally done with this one too.

--- palindrome.py
+++ palindrome.py
@@ -2,21 +2,20 @@
 #Palindrom is something who have the same result if reversed for e.g. " MOM "
 #how do we know whether the given string or number is palindrome
 number = 0
-number = int(input("Please Enter the number to Check whether it is a palidrome : "))
 reverse = 0
 temp = 0
+number = int(input("Please Enter the number to Check whether it is a palidrome : "))
+print(number)
+temp = number #given number is copied in temp variable
+
+while(temp != 0):                       # until the number is reversed
+    reverse = reverse * 10              # reverse will store the reversed number by one digit and shift the place of digit
+    reverse = reverse + temp % 10       # remainder of the given number gives last digit every time and is stored in reverse
+    temp = int(temp/10)                 # every last digit of given number is eliminated here 
+if(reverse == number):
+    print("Number is a Palindrome")
+    
+else:
+    print("Number is not a Palindrome")
 
 
-temp = number
-
-while(reverse<=number):
-    reverse = reverse + temp % 10    # 1221 revrse = 1       reverse = 12    reverse 122         reverse = 1221
-    if(reverse >= number):
-        print(reverse)
-        exit()
-    reverse = reverse * 10  # reverse = 10                  reverse = 120   reverse 1220
-    temp = int(temp/10)     # temp = 122                    temp = 12       temp = 1
-
-
-
-#print(reverse)
