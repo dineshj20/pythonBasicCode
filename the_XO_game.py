96ed7507d0e5e079284fb2397922e0ad57134cd7
Dinesh Jain <cooldj14@gmail.com>
2020-02-07 15:02:21 +0100

    SUCCESSFULLY USED MAIN CLASS

--- the_XO_game.py
+++ the_XO_game.py
@@ -1,31 +1,53 @@
-box =  ['_','_','_','_','_','_',' ',' ',' '] #this variable is used to create the matrix for the game
-position_filled = [1]
+class main:
+    box =  ['_','_','_','_','_','_',' ',' ',' '] #this variable is used to create the matrix for the game
+    position_filled = []
+    player1 = 0
+    player2 = 0
+m1 = main()
 def print_matrix():
-    print (box)
-    for index, item in enumerate(box, start=1):
+    print (m1.box)
+    for index, item in enumerate(m1.box, start=1):
         print(item, end='|' if index % 3 else '\n')
+    return(m1.box)
 print("Hello Players ('|') Lets Play")
 
-
-
 def p1():
-    print("Hello Player1")
-    i = int(input("Player1 Enter the box number from 1 to 9 "))
-    c1(i)
+    while m1.player1 < 1:
+        print("Hello Player1")
+        i = int(input("Player1 Enter the box number from 1 to 9 "))
+        c1(i)
     #chara = 'X'
     #update_a(a, i, chara)
     print_matrix()
 
+def p2():
+    while m1.player2 < 1:
+        print("Hello Player2")
+        i = int(input("Player2 Enter the box number from 1 to 9 "))
+        c2(i)
+    #chara = 'X'
+    #update_a(a, i, chara)
+    print_matrix()
 
 def c1(i):
-    if i in position_filled:
+    if i in m1.position_filled:
         print("Already entered")
-        p1()
+    else:
+        m1.position_filled.append(i)
+        m1.box[i-1] = 'X'
+        m1.player1 = 2
+
+
+def c2(i):
+    if i in m1.position_filled:
+        print("Already entered")
 
     else:
-        position_filled.append(i)
-        box[i-1] = 'X'
+        m1.position_filled.append(i)
+        m1.box[i-1] = 'O'
+        m1.player2 = 2
 
 
 print_matrix()
 p1()
+p2()
