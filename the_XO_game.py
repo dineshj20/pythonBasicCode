7ca26563267c15226fe8360dd70109a68b28a13b
Dinesh Jain <cooldj14@gmail.com>
2020-04-18 23:45:27 +0200

    xo game

--- the_XO_game.py
+++ the_XO_game.py
@@ -5,16 +5,20 @@ class main:
     player2 = 0
 m1 = main()
 def print_matrix():
-    print (m1.box)
     for index, item in enumerate(m1.box, start=1):
         print(item, end='|' if index % 3 else '\n')
     return(m1.box)
 print("Hello Players ('|') Lets Play")
 
-def p1():
+def p1(): #here we take the input of player1 and check whether the place is empty or is the input correct
     while m1.player1 < 1:
         print("Hello Player1")
-        i = int(input("Player1 Enter the box number from 1 to 9 "))
+        i = 0
+        while 1 > i or 9 < i:
+            try:
+                i = int(input("Player1 Enter the box number from 1 to 9 "))
+            except ValueError:
+                print ("wooow that was not a number:(")
         c1(i)
     #chara = 'X'
     #update_a(a, i, chara)
@@ -23,7 +27,12 @@ def p1():
 def p2():
     while m1.player2 < 1:
         print("Hello Player2")
-        i = int(input("Player2 Enter the box number from 1 to 9 "))
+        i = 0
+        while 1 > i or 9 < i:
+            try:
+                i = int(input("Player2 Enter the box number from 1 to 9 "))
+            except ValueError:
+                print ("wooow that was not a number:(")
         c2(i)
     #chara = 'X'
     #update_a(a, i, chara)
